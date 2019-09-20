43367605171aa5c9829a3ef84c002d768c37a62c
aaron-plahn <aplahn85@gmail.com>
2019-09-20 11:55:48 -0700

    Generated tile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from '@src/app/app-routing.module';
 import { AppComponent } from '@src/app/app.component';
 import { HomeComponent } from '@src/app/home/home.component';
+import { TileComponent } from '@src/app/tile/tile.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    TileComponent,
   ],
   imports: [
     BrowserModule,
